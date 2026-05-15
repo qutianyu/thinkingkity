4dbdeee06e80d436427eb1f74b5e7e472a9cf7c7
qutianyu <mikylin@aliyun.com>
2026-05-15 22:51:35 +0800

    v0.7.1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,3 +4,53 @@ pub mod global_config;
 pub mod server;
 pub mod sync_common;
 pub mod sync_git;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![
+            commands::read_directory,
+            commands::read_file,
+            commands::read_file_base64,
+            commands::write_file,
+            commands::write_file_base64,
+            commands::write_vault_markdown_file,
+            commands::browse_page_with_playwright,
+            commands::create_file,
+            commands::create_folder,
+            commands::copy_file,
+            commands::rename_file,
+            commands::delete_file,
+            commands::get_vault_size,
+            commands::create_snapshot,
+            commands::list_snapshots,
+            commands::read_snapshot,
+            commands::restore_snapshot,
+            commands::move_to_trash,
+            commands::list_trash,
+            commands::restore_trash,
+            commands::delete_trash_entry,
+            commands::list_vaults,
+            sync_git::sync_git_init,
+            sync_git::sync_git_sync,
+            global_config::read_global_vaults,
+            global_config::write_global_vaults,
+            global_config::read_login_status,
+            global_config::verify_login,
+            global_config::logout,
+            global_config::read_allowed_paths,
+            global_config::write_allowed_paths,
+            global_config::ensure_demo_vault,
+            global_config::ensure_allowed_path_cmd,
+        ])
+        .run(tauri::generate_context!())
+        .unwrap_or_else(|e| {
+            eprintln!("Fatal: {}", e);
+            std::process::exit(1);
+        });
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,50 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![
-            thinkingkity::commands::read_directory,
-            thinkingkity::commands::read_file,
-            thinkingkity::commands::read_file_base64,
-            thinkingkity::commands::write_file,
-            thinkingkity::commands::write_file_base64,
-            thinkingkity::commands::write_vault_markdown_file,
-            thinkingkity::commands::browse_page_with_playwright,
-            thinkingkity::commands::create_file,
-            thinkingkity::commands::create_folder,
-            thinkingkity::commands::copy_file,
-            thinkingkity::commands::rename_file,
-            thinkingkity::commands::delete_file,
-            thinkingkity::commands::get_vault_size,
-            thinkingkity::commands::create_snapshot,
-            thinkingkity::commands::list_snapshots,
-            thinkingkity::commands::read_snapshot,
-            thinkingkity::commands::restore_snapshot,
-            thinkingkity::commands::move_to_trash,
-            thinkingkity::commands::list_trash,
-            thinkingkity::commands::restore_trash,
-            thinkingkity::commands::delete_trash_entry,
-            thinkingkity::commands::list_vaults,
-            thinkingkity::sync_git::sync_git_init,
-            thinkingkity::sync_git::sync_git_sync,
-            thinkingkity::global_config::read_global_vaults,
-            thinkingkity::global_config::write_global_vaults,
-            thinkingkity::global_config::read_login_status,
-            thinkingkity::global_config::verify_login,
-            thinkingkity::global_config::logout,
-            thinkingkity::global_config::read_allowed_paths,
-            thinkingkity::global_config::write_allowed_paths,
-            thinkingkity::global_config::ensure_demo_vault,
-            thinkingkity::global_config::ensure_allowed_path_cmd,
-        ])
-        .run(tauri::generate_context!())
-        .unwrap_or_else(|e| {
-            eprintln!("Fatal: {}", e);
-            std::process::exit(1);
-        });
+    thinkingkity::run();
 }
